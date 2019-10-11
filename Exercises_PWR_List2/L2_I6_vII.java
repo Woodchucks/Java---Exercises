17f84d662505f9130784a0f0e706948677e5909a
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2019-10-11 12:22:49 +0200

    Create L2_I6_vII.java

---
+++ Exercises_PWR_List2/L2_I6_vII.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class L2_D6 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Program szukający największy wspólny podzielnik liczb naturalnych. Podaj dwie liczby naturalne");
+		long M = s.nextLong();
+		long N = s.nextLong();
+		long x = M%N;
+		while (x!=0){
+			M = N;
+			N = x;
+			x = M%N;
+		} //while
+		System.out.println("Największy wpólny dzielnik liczb wynosi " + N);
+		s.close();
+	} //main
+} //L2_D6
