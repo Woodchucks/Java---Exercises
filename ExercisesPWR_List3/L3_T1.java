d9781afd4a0792bdf9344b8f32c13d678c237a3a
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2019-10-12 14:21:22 +0200

    Create L3_T1.java

---
+++ ExercisesPWR_List3/L3_T1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class L3_T1 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Podaj przedział k liczb całkowitych oraz liczebność zbioru elementu n");
+		int k = s.nextInt();
+		int n = s.nextInt();
+		int[] ciag = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			ciag[i] = (int)(Math.random()*k +1);
+		}
+		
+		int[] parz = new int[n];
+		int[] nieparz = new int[n];
+		int lp, ln;
+		lp = 0; 
+		ln = 0;
+			
+		for(int i=0; i<n; i++) {
+			if(ciag[i]%2 == 0) parz[lp++] = ciag[i];
+			else nieparz[ln++] = ciag[i];
+		}
+		
+		System.out.print("Liczby parzyste: ");
+		for(int i=0; i<lp; i++) {
+			System.out.print(parz[i] + ",");
+		}
+		
+		System.out.println();
+		
+		System.out.print("Liczby nieparzyste: ");
+		for(int i=0; i<ln; i++) {
+			System.out.print(nieparz[i] + ",");
+		}
+		
+		System.out.println();
+		s.close();
+	} //main
+} //L3_T1
