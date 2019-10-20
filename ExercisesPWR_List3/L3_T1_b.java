800b8035a59bef35f916222e91846583fe2a38af
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2019-10-20 14:35:44 +0200

    Update L3_T1_b.java

--- ExercisesPWR_List3/L3_T1_b.java
+++ ExercisesPWR_List3/L3_T1_b.java
@@ -19,6 +19,26 @@ public class L3_T1_b {
 		for(int i=0; i<n; i++) {
 			System.out.print(zbior[i] + " ");		
 		}
+		System.out.println();
+		boolean powtorz;
+		int krok = 0;
+		do {
+			powtorz = false;
+			for(int i=0; i<n-krok-1; i++) {
+				if(zbior[i]>zbior[i+1]) {	
+					int p = zbior[i];
+					zbior[i] = zbior[i+1];
+					zbior[i+1] = p;
+					powtorz = true;
+				} //if
+			} //for
+			krok++;
+		} //do
+		while(powtorz);			//dopoki powtorz bedzie true 
+		
+		for(int i=0; i<n; i++) {
+			System.out.print(zbior[i] + " ");	
+		}
 		s.close();
 	} //main
-} //L3_T1_b6
+} //L3_T1_b
