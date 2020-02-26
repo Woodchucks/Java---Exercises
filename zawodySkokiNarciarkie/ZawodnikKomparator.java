c4bf7b8637dd237589a0cf2405ef650f8ddead50
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2020-02-26 10:40:57 +0100

    Create ZawodnikKomparator.java

---
+++ zawodySkokiNarciarkie/ZawodnikKomparator.java
@@ -0,0 +1,6 @@
+public class ZawodnikKomparator {
+    public boolean compare(Zawodnik zawodnik1, Zawodnik zawodnik2) {
+        // Integer.valueOf(zawodnik1.getPunkty()).compareTo(zawodnik2.getPunkty()); //jesli 1 wart wieksza od 2 return da 1 zawodnika
+        return false;
+    }
+}
