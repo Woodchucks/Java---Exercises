2370714464a2b07372d84ee3bb437bbb5727009a
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2020-03-21 12:04:26 +0100

    Create Test.java

---
+++ realizacjaFaktur/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        Faktura faktura1 = new Faktura("gotówka", "25.02.2019");
+        System.out.println(faktura1.wyswietlFakture()); 
+    }
+}
