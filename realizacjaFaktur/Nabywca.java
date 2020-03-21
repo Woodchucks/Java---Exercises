4775b3d593ee84387f3663b2ac8f5e73c52b5d8d
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2020-03-21 12:04:06 +0100

    Create Nabywca.java

---
+++ realizacjaFaktur/Nabywca.java
@@ -0,0 +1,13 @@
+public class Nabywca {
+    String imie, nazwisko, adres, eMail;
+    int wiek, nrTel;
+
+    public Nabywca(String imie, String nazwisko, String adres, String eMail, int wiek, int nrTel) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.adres = adres;
+        this.eMail = eMail;
+        this.wiek = wiek;
+        this.nrTel = nrTel;
+    }
+}
