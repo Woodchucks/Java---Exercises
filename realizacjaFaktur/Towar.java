66b63349926dce5b3c7f38fd5d0879e8d77532f6
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2020-03-21 12:04:43 +0100

    Create Towar.java

---
+++ realizacjaFaktur/Towar.java
@@ -0,0 +1,11 @@
+public class Towar {
+    String nazwa, kategoria;
+    long kodKreskowy;
+    double cena;
+    public Towar(String nazwa, String kategoria, long kodKreskowy, double cena) {
+        this.nazwa = nazwa;
+        this.kategoria = kategoria;
+        this.kodKreskowy = kodKreskowy;
+        this.cena = cena;
+    }
+}
